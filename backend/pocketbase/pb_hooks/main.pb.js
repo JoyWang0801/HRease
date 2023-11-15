ffb0fdedb133cb570b709c4f02af06faffe0f3b1
Joy Wang <yihan.wang3@ucalgary.ca>
2023-11-14 22:36:08 -0700

    - routing works

--- backend/pocketbase/pb_hooks/main.pb.js
+++ backend/pocketbase/pb_hooks/main.pb.js
@@ -4,6 +4,17 @@ routerAdd("GET", "/hello/:name", (c) => {
     return c.json(200, { "message": "Hello " + name })
 })
 
+routerAdd("GET", "/testRoute", (c) => {
+    //let name = c.pathParam("name")
+
+    return c.json(200, { "message": "Hello "})
+})
+
+routerAdd("POST", "/ohgg", (c) => {
+    //let name = c.pathParam("name")
+
+    return c.json(200, { "message": "Ohgg "})
+})
 onModelAfterUpdate((e) => {
     console.log("user updated...", e.model.get("email"))
 }, "users")
--- frontend/src/AddUserPage.js
+++ frontend/src/AddUserPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Button, TextField, Container, Typography, MenuItem, styled, Card} from '@mui/material';
 import Paper from '@mui/material/Paper';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -9,6 +9,7 @@ import Box from '@mui/material/Box';
 import pb from "./lib/pocketbase";
 import Stack from '@mui/material/Stack';
 import {Form, useForm} from "react-hook-form";
+import axios from 'axios';
 
 
 const DemoPaper = styled(Card)(({ theme }) => ({
@@ -18,278 +19,6 @@ const DemoPaper = styled(Card)(({ theme }) => ({
     ...theme.typography.body2,
     textAlign: 'center'
 }));
-
-/*
-<DemoPaper elevation={6} square={false}>
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        Contact Info
-                    </Typography>
-                    <Container component="main" maxWidth="xs">
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    autoComplete="Email"
-                                    name="email"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email"
-                                    type="text"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    autoComplete="Phone Number"
-                                    name="phoneNumber"
-                                    required
-                                    fullWidth
-                                    id="phoneNumber"
-                                    label="Phone Number"
-                                    type="text"
-                                    autoFocus
-                                />
-                            </Grid>
-                        </Grid>
-                        <Grid container justifyContent="center">
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Container>
-                </Box>
-
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        Emergency Contact
-                    </Typography>
-                    <Container component="main" maxWidth="xs">
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="Relationship"
-                                    name="relationship"
-                                    id="relationship"
-                                    label="Relationship"
-                                    type="text"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="Full Name"
-                                    name="fullName"
-                                    id="fullName"
-                                    label="Full Name"
-                                    type="text"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="Email"
-                                    name="email"
-                                    id="email"
-                                    label="Email"
-                                    type="text"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="Phone Number"
-                                    name="phoneNumber"
-                                    id="phoneNumber"
-                                    label="Phone Number"
-                                    type="text"
-                                    autoFocus
-                                />
-                            </Grid>
-                        </Grid>
-
-                        <Grid container justifyContent="center">
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-
-                    </Container>
-                </Box>
-            </DemoPaper>
- */
-
-
-
-/*PERSONAL INFO FORM*/
-/*
-<Box
-                        sx={{
-                            marginTop: 8,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Typography component="h1" variant="h5">
-                            Personal Info
-                        </Typography>
-                        <Container component="main" maxWidth="xs">
-                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        autoComplete="given-name"
-                                        name="firstName"
-                                        required
-                                        fullWidth
-                                        id="firstName"
-                                        label="First Name"
-                                        type="text"
-                                        autoFocus
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="lastName"
-                                        label="Last Name"
-                                        name="lastName"
-                                        type="string"
-                                        autoComplete="family-name"
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="address"
-                                        label="Address"
-                                        name="address"
-                                        type="string"
-                                        autoComplete="address"
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        name="password"
-                                        label="Password"
-                                        type="password"
-                                        id="password"
-                                        autoComplete="new-password"
-                                    />
-                                </Grid>
-
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="dateOfBirth"
-                                        helperText="Date of Birth"
-                                        name="dateOfBirth"
-                                        type="date"
-                                    >
-                                    </TextField>
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        id="gender"
-                                        select
-                                        required
-                                        label="Gender"
-                                        name="gender"
-                                        defaultValue="NA"
-                                        helperText="Please select your gender"
-                                    >
-                                        {genders.map((option) => (
-                                            <MenuItem key={option.value} value={option.value}>
-                                                {option.label}
-                                            </MenuItem>
-                                        ))}
-                                    </TextField>
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        autoComplete="city"
-                                        name="city"
-                                        required
-                                        fullWidth
-                                        id="city"
-                                        type="string"
-                                        label="City"
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="province"
-                                        label="Province"
-                                        name="province"
-                                        autoComplete="province"
-                                        type="string"
-                                        autoFocus
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="postCode"
-                                        label="Post Code"
-                                        name="postCode"
-                                        autoComplete="postCode"
-                                        autoFocus
-                                        type="string"
-                                    />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <FormControlLabel
-                                        control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                        label="I want to receive inspiration, marketing promotions and updates via email."
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign Up
-                            </Button>
-                            <Grid container justifyContent="center">
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        Already have an account? Sign in
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                        </Container>
-                    </Box>
- */
 
 const genders = [
     {
@@ -333,8 +62,17 @@ const AddUserPage = () => {
             //     //ID, Name, Gender, SSN, Emergency Contact, Department, Position, Avatar, Starting Date, Bank Account.
             // };
 
-            const record = await pb.collection('test').create(data);
-            console.log(record);
+            //const record = await pb.collection('test').create(data);
+            //console.log(record);
+
+            try {
+                let name = "sana";
+                const response = await axios.get(pb.baseUrl + `/hello/${name}`);
+                console.log(response);
+            } catch (error) {
+                console.error('Error fetching file content:', error);
+            }
+
         } catch (e) {
             console.log(e);
         }
@@ -537,7 +275,7 @@ const AddUserPage = () => {
                                         <TextField
                                             autoComplete="Email"
                                             label="Email"
-                                            type="text"
+                                            type="email"
                                             autoFocus
                                             {...register("dependentEmail")}
                                         />
@@ -549,7 +287,6 @@ const AddUserPage = () => {
                                             type="text"
                                             autoFocus
                                             {...register("dependentPhoneNumber")}
-
                                         />
                                     </Grid>
                                 </Grid>
