746ed55f95e15693785b62b9c5eb65678c772d1d
Sjnaka <wu.dingkai@hotmail.com>
2023-12-06 13:18:36 -0700

    fetch dashboard info from db

---
+++ frontend/src/components/styles/PersonalView.style.js
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+
+export const GreenBox = styled.div `
+    width: 100%;
+    height: 100%;
+    background-color: var(--hre-light-green);
+    border: 3px solid var(--hre-dark-green);
+    border-radius: 10px;
+`;
+
+export const TopWrapper = styled.div `
+    display: flex;
+    flex-direction: row;
+    // justify-content: space-evenly;
+    gap: 50px;
+`;
+
+export const Time = styled.div `
+    width: 20%;
+    margin-top: 30px;
+    font-size: 100px;
+    flex: 1;
+`;
+
+export const ClockInOutButton = styled.div `
+    background-color: var(--hre-dark-green);
+    color: white;
+    text-align: center;
+    padding: 5%;
+    border-radius: 10px;
+    box-shadow: 1px 1px 5px grey;
+    font-size: 32px;
+    display: flex;
+    align-items: center;
+    flex: 1;
+`;
+
+export const ProfileCard = styled.div `
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+`;
+
+export const ContainerWrapper = styled.div `
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    margin-top: 100px;
+    width: 1000px;
+    align-items: center;
+    gap: 100px;
+`;
+
+export const EmployeeName = styled.div `
+    font-size: 48px;
+    font-weight: 800;
+`;
+
+export const ProfilePic = styled.img `
+    width: 175px;
+`;
+
+export const EmployeeRole = styled.div `
+`;
+
+export const EmployeePronouns = styled.div `
+    font-weight: bold;
+`;
+
+export const ProfileCardInfoWrapper = styled.div `
+    display: flex;
+    flex-direction: column;
+    font-size: 22px;
+    flex: 3;
+    padding-right: 20px;
+    margin-right: 50px;
+`;
+
+export const UL = styled.ul `
+    list-style: none;
+    font-size: 32px;
+    margin-top: 60px;
+`;
+
+export const LI = styled.li `
+    margin-bottom: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const Label = styled.div `
+`;
+
+export const Info = styled.div `
+    margin-right: 40px;
+`;
